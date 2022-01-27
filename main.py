10e2409a8deaec9977054acd7dc21a31b3694270
jaykhans <jahongirnagmatov@gmail.com>
2022-01-27 11:03:17 +0500

    include code in function in main and update

--- main.py
+++ main.py
@@ -1,17 +1,21 @@
 from question_model import Question
-from data import question_data2
+from data import question_data_1, question_data_2
 from quiz_brain import QuizBrain
 
 question_bank = []
 
-for question in question_data2:
-    new_question = Question(question["question"], question["correct_answer"])
+def main():
+    for question in question_data_2:
+        new_question = Question(question["question"], question["correct_answer"])
 
-    question_bank.append(new_question)
+        question_bank.append(new_question)
 
-quiz = QuizBrain(question_bank)
+    quiz = QuizBrain(question_bank)
 
-while quiz.still_has_questions():
-    quiz.next_question()
-print("You've completed the quiz!")
-print(f"Your final score was: {quiz.score}/{quiz.question_number}")
+    while quiz.still_has_questions():
+        quiz.next_question()
+    print("You've completed the quiz!")
+    print(f"Your final score was: {quiz.score}/{quiz.question_number}")
+
+
+main()
